4a57790b31d13c3fae2fd9f0532ec9495fcd70b3
DanielPinigin <danielpinigin@hanovernorwichschools.org>
2017-03-24 14:57:23 -0400

    Updated from Brython Server: 3/24/2017 2:57:23 PM

--- distribution.py
+++ distribution.py
@@ -54,12 +54,12 @@ thelist = [sent1.count(c) for c in string.ascii_lowercase]
 alph = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 #alph1 = alph.reverse()
 
-z = list(zip(alph,thelist))
+z = list(zip(thelist,alph))
 #print(z)
 zs = sorted(z, reverse = True)
 #print(zs)
 
-alph, thelist=zip(*zs)
+thelist, alph=zip(*zs)
 #print(alph)
 
 for x in alph:
