3a6a6eabb8b9db4173f52781137cbb4c3bf3dadb
DanielPinigin <danielpinigin@hanovernorwichschools.org>
2017-03-24 14:21:58 -0400

    Updated from Brython Server: 3/24/2017 2:21:58 PM

--- distribution.py
+++ distribution.py
@@ -52,10 +52,9 @@ thelist = [sent1.count(c) for c in string.ascii_lowercase]
 print(thelist)
 
 numb = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26']
-z = zip(thelist,numb)
-print(list(zip(thelist,numb)))
+z = list(zip(thelist,numb))
+print(z)
 
-v = [x for (y,x) in sorted(zip(thelist,numb), key=lambda pair: pair[0])] 
-print(v)
+
 
 #'a string has several characters'.count('c')
